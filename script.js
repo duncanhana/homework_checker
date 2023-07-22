9ec2e26b449930ded28eed716527c86705193d7b
duncanhana <140259070+duncanhana@users.noreply.github.com>
2023-07-23 00:45:00 +1000

    Update script.js

--- script.js
+++ script.js
@@ -26,4 +26,43 @@ users.forEach(function(user) {
     userDiv.appendChild(progressBar);
 
     userList.appendChild(userDiv);
+
+    var nameInput = document.getElementById('nameInput');
+var addButton = document.getElementById('addButton');
+
+addButton.addEventListener('click', function() {
+    var name = nameInput.value;
+    if (name) {
+        users.push({ name: name, progress: 0 });
+        nameInput.value = '';
+        renderUsers();
+    }
+});
+
+function renderUsers() {
+    userList.innerHTML = '';
+    users.forEach(function(user) {
+        var userDiv = document.createElement('div');
+        userDiv.className = 'user';
+
+        var userName = document.createElement('div');
+        userName.textContent = user.name;
+        userDiv.appendChild(userName);
+
+        var progressBar = document.createElement('div');
+        progressBar.className = 'progress-bar';
+
+        var progressBarInner = document.createElement('div');
+        progressBarInner.className = 'progress-bar-inner';
+        progressBarInner.style.width = user.progress + '%';
+        progressBar.appendChild(progressBarInner);
+
+        userDiv.appendChild(progressBar);
+
+        userList.appendChild(userDiv);
+    });
+}
+
+renderUsers();
+
 });
